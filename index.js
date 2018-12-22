4e8221fcd4912eacd7ba432f0897c1f019c8d11a
Robert Snyder <r.snyder49@gmail.com>
2018-12-22 20:58:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,4 +5,12 @@ function driversWithRevenueOver(drivers, rev) {
   return drivers.filter(function(driver) {
      return driver.revenue > rev;
    });
- }
+}
+
+function driverNamesWithRevenueOver(drivers, rev) {
+  return driversWithRevenueOver(drivers, rev).map(
+     function(driver) {
+       return driver.name;
+     });
+} 
+
