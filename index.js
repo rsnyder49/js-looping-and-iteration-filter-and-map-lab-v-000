aa50af029811a3f2b155b797d9ad1cc6011afadb
Robert Snyder <r.snyder49@gmail.com>
2018-12-22 20:54:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 // Code your solution here:
 const drivers = [];
+
+function driversWithRevenueOver(drivers, rev) {
+  return drivers.filter(function(driver){
+     return driver.revenue > rev;
+   });
+ }
