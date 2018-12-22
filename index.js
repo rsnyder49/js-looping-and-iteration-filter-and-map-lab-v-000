1f8fa65cfe25f4e9833854feb7dc23d7bfb5eda6
Robert Snyder <r.snyder49@gmail.com>
2018-12-22 21:04:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,28 +3,29 @@ const drivers = [];
 
 function driversWithRevenueOver(drivers, rev) {
   return drivers.filter(function(driver) {
-     return driver.revenue > rev;
+    return driver.revenue > rev;
    });
 }
 
 function driverNamesWithRevenueOver(drivers, rev) {
   return driversWithRevenueOver(drivers, rev).map(
-     function(driver) {
-       return driver.name;
+    function(driver) {
+      return driver.name;
      });
 } 
 
 function exactMatch(drivers, cond) {
   const metCond = drivers.filter(function(d) {
-     return ((cond.name === d.name) || (cond.revenue === d.revenue));
+    return ((cond.name === d.name) || (cond.revenue === d.revenue));
    });
+   
    return metCond;
 }
 
 function exactMatchToList(extendedDrivers, cond) {
   return exactMatch(extendedDrivers, cond).map(
-     function(d) {
-       return d.name;
+    function(d) {
+      return d.name;
      }
    );
 }
